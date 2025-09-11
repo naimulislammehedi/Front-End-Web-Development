78511a52fed3386b5329a8a0be858eb09549ff07
Mehedi <mehedi.interface@gmail.com>
2025-09-11 10:02:26 +0300

    module 4.2

--- Module 4 - JavaScript - Making the Web Interactive/4.1. JS Fundamentals/script.js
+++ Module 4 - JavaScript - Making the Web Interactive/4.1. JS Fundamentals/script.js
@@ -1,4 +1,26 @@
-let myName = "Mehedi"; 
-for (let i = 0; i < 5; i++) {
-    console.log(myName)
-}
+// let person = {
+//     name: "Mehedi", 
+//     age: 25, 
+//     country: "Bangladesh"
+// }; 
+
+// console.log(person.name); 
+// console.log(person["age"]); 
+
+// person.age = 26; 
+
+// person.job = "Engineer"; 
+
+// console.log(person)
+
+let car = {
+    brand: "Toyota", 
+    model: "Corolla", 
+    year: 2020
+}; 
+
+console.log(car.brand); 
+
+car.year = 2023; 
+car.color = "White"; 
+console.log(car)
